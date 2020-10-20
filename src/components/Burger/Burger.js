ebba3cb5f2effa9b1cadbceea567778a0ce2917c
saba-nawaz <saba135777@gmail.com>
2020-10-20 14:35:47 +0500

    addRemoveHandler

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,31 @@ import BurgerBuilder from '../../containors/BurgerBuilder/BurgerBuilder';
 import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredients/BurgerIngredients'
 const burger = (props) => {
+    let transformedIngredients = Object.keys(props.ingredients)
+    
+    .map(igKey => {
+    //converted into array but withoiut values
+ 
+        return[...Array(props.ingredients[igKey])].map((_, i) => {
+            return <BurgerIngredient key={igKey + i} type={igKey} />;
+           
+        });
+    
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el);
+    },[]);
+    if (transformedIngredients.length === 0)
+    {
+        transformedIngredients = <p>Please start ading ingredients</p>
+
+    }
+    console.log(transformedIngredients)
     return (
         <div className={classes.Burger}>
-
+ 
 <BurgerIngredient type="bread-top"/>
-<BurgerIngredient type="bread-bottom"/>
-<BurgerIngredient type="cheese"/>
-<BurgerIngredient type="meat"/>
+{transformedIngredients}
 <BurgerIngredient type="bread-bottom"/>
 
         </div>
